e3f0124631cc57282196df8d96a1757a46a858d9
giim <victor.oriedreh@gmail.com>
2016-07-17 21:19:46 +0100

    Fixed bugs

--- guiSearch.py
+++ guiSearch.py
@@ -121,6 +121,5 @@ class SearchGui(QWidget):
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	ex = SearchGui()
-	#sys.exit(app.exec_())
-	app.exec_()
+	sys.exit(app.exec_())
 	# clearFolder()
